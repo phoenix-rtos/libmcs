8d52876f537700d3e2ddf5a43e093227832b3d89
Thomas Wucher <thomas.wucher@gtd-gmbh.de>
2023-04-19 16:00:15 +0200

    Provide minimal fenv for core-math

--- libm/include/fenv.h
+++ libm/include/fenv.h
@@ -4,16 +4,39 @@
 #ifndef LIBMCS_FENV_H
 #define LIBMCS_FENV_H
 
-#error fenv.h/fenv.c shall not be used as is. They have no functionality \
-       other than returning an error value and providing prototypes.     \
-       If you, the user, want to use fenv you will have to implement the \
-       features yourself (or copy them from somewhere). We can not       \
-       provide these functionalities for you as their implementation is  \
-       highly platform dependant.
-
 #ifdef __cplusplus
 extern "C"{
 #endif
+
+#define FE_TONEAREST  0
+#define FE_INVALID    1
+#define FE_DOWNWARD   1024
+#define FE_UPWARD     2048
+#define FE_TOWARDZERO 3072
+
+/* Type representing exception flags.  */
+typedef unsigned short int fexcept_t;
+
+typedef struct
+{
+    unsigned short int __control_word;
+    unsigned short int __glibc_reserved1;
+    unsigned short int __status_word;
+    unsigned short int __glibc_reserved2;
+    unsigned short int __tags;
+    unsigned short int __glibc_reserved3;
+    unsigned int __eip;
+    unsigned short int __cs_selector;
+    unsigned int __opcode:11;
+    unsigned int __glibc_reserved4:5;
+    unsigned int __data_offset;
+    unsigned short int __data_selector;
+    unsigned short int __glibc_reserved5;
+#ifdef __x86_64__
+    unsigned int __mxcsr;
+#endif
+}
+fenv_t;
 
 /* Floating-point Exceptions */
 extern int feclearexcept(int);
