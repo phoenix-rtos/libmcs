95c2c63973bd54c8f3a848af00ef42fadc31c2f7
Andoni Arregi <andoni.arregui@gtd-gmbh.de>
2023-11-23 08:56:18 +0100

    Rework integer types in trunc

--- libm/mathf/truncf.c
+++ libm/mathf/truncf.c
@@ -10,8 +10,8 @@ float truncf(float x)
     x *= __volatile_onef;
 #endif /* defined(__LIBMCS_FPU_DAZ) */
 
-    int32_t sb;
-    int32_t w;
+    uint32_t sb;
+    uint32_t w;
     int32_t exponent_less_127;
 
     GET_FLOAT_WORD(w, x);
@@ -20,14 +20,14 @@ float truncf(float x)
     sb = w & 0x80000000U;
 
     /* Extract exponent field. */
-    exponent_less_127 = ((w & 0x7f800000) >> 23) - 127;
+    exponent_less_127 = (int32_t)((w & 0x7f800000U) >> 23U) - 127;
 
     if (exponent_less_127 < 23) {
         if (exponent_less_127 < 0) {
             /* -1 < x < 1, so result is +0 or -0. */
             SET_FLOAT_WORD(x, sb);
         } else {
-            SET_FLOAT_WORD(x, sb | (w & ~(0x007fffff >> exponent_less_127)));
+          SET_FLOAT_WORD(x, sb | (w & ~(0x007fffffU >> (uint32_t)exponent_less_127)));
         }
     } else {
         if (exponent_less_127 == 128)
