531b8ea25720cfa1267a69439f361489bb821e7d
Andoni Arregi <andoni.arregui@gtd-gmbh.de>
2024-01-25 11:53:50 +0100

    Rework integer types in round

--- libm/mathf/roundf.c
+++ libm/mathf/roundf.c
@@ -10,40 +10,33 @@ float roundf(float x)
     x *= __volatile_onef;
 #endif /* defined(__LIBMCS_FPU_DAZ) */
 
-    uint32_t w;
-    /* Most significant word, least significant word. */
+    uint32_t w, exponent_mask;
     int32_t exponent_less_127;
 
     GET_FLOAT_WORD(w, x);
 
     /* Extract exponent field. */
-    exponent_less_127 = (int32_t)((w & 0x7f800000) >> 23) - 127;
+    exponent_less_127 = (int32_t)((w & 0x7f800000U) >> 23U) - 127;
 
     if (exponent_less_127 < 23) {
         if (exponent_less_127 < 0) {
             w &= 0x80000000U;
 
-            if (exponent_less_127 == -1) {
-                /* Result is +1.0 or -1.0. */
-
-                w |= ((uint32_t)127 << 23);
+            if (exponent_less_127 == -1) {                  /* Result is +1.0 or -1.0. */
+                w |= ((uint32_t)127U << 23U);
             }
         } else {
-            uint32_t exponent_mask = 0x007fffff >> exponent_less_127;
+            exponent_mask = 0x007fffffU >> (uint32_t)exponent_less_127;
 
-            if ((w & exponent_mask) == 0) {
-                /* x has an integral value. */
-
+            if ((w & exponent_mask) == 0U) {                 /* x has an integral value. */
                 return x;
             }
 
-            w += 0x00400000 >> exponent_less_127;
+            w += 0x00400000U >> (uint32_t)exponent_less_127;
             w &= ~exponent_mask;
         }
     } else {
-        if (exponent_less_127 == 128) {
-            /* x is NaN or infinite. */
-
+        if (exponent_less_127 == 128) {                     /* x is NaN or infinite. */
             return x + x;
         } else {
             return x;
