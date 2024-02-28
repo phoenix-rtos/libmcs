f7f9ffc603385d4ed09f1d1d82d0efa9b3b140fb
Christoph Weiss <christoph.weiss@gtd-gmbh.de>
2024-02-28 11:17:01 +0100

    Rework integer types in cbrt

--- libm/mathd/cbrtd.c
+++ libm/mathd/cbrtd.c
@@ -54,8 +54,8 @@
 #ifndef __LIBMCS_DOUBLE_IS_32BITS
 
 static const uint32_t
-B1 = 715094163, /* B1 = (682-0.03306235651)*2**20 */
-B2 = 696219795; /* B2 = (664-0.03306235651)*2**20 */
+B1 = 715094163U, /* B1 = (682-0.03306235651)*2**20 */
+B2 = 696219795U; /* B2 = (664-0.03306235651)*2**20 */
 
 static const double
 C  =  5.42857142857142815906e-01, /* 19/35     = 0x3FE15F15, 0xF15F15F1 */
@@ -70,37 +70,34 @@ double cbrt(double x)
     x *= __volatile_one;
 #endif /* defined(__LIBMCS_FPU_DAZ) */
 
-    int32_t hx;
     double r, s, t = 0.0, w;
-    uint32_t sign;
-    uint32_t high, low;
+    uint32_t hx, sign, high, low;
 
     GET_HIGH_WORD(hx, x);
     sign = hx & 0x80000000U;     /* sign= sign(x) */
     hx  ^= sign;
 
-    if (hx >= 0x7ff00000) {
+    if (hx >= 0x7ff00000U) {
         return (x + x);    /* cbrt(NaN,INF) is itself */
     }
 
     GET_LOW_WORD(low, x);
 
-    if ((hx | low) == 0) {
+    if ((hx | low) == 0U) {
         return (x);    /* cbrt(0) is itself */
     }
 
     SET_HIGH_WORD(x, hx);   /* x <- |x| */
 
     /* rough cbrt to 5 bits */
-    if (hx < 0x00100000) {    /* subnormal number */
-        SET_HIGH_WORD(t, 0x43500000);   /* set t= 2**54 */
+    if (hx < 0x00100000U) {    /* subnormal number */
+        SET_HIGH_WORD(t, 0x43500000U);   /* set t= 2**54 */
         t *= x;
         GET_HIGH_WORD(high, t);
-        SET_HIGH_WORD(t, high / 3 + B2);
+        SET_HIGH_WORD(t, high / 3U + B2);
     } else {
-        SET_HIGH_WORD(t, hx / 3 + B1);
+        SET_HIGH_WORD(t, hx / 3U + B1);
     }
-
 
     /* new cbrt to 23 bits, may be implemented in single precision */
     r = t * t / x;
@@ -109,8 +106,7 @@ double cbrt(double x)
 
     /* chopped to 20 bits and make it larger than cbrt(x) */
     GET_HIGH_WORD(high, t);
-    INSERT_WORDS(t, high + 0x00000001, 0);
-
+    INSERT_WORDS(t, high + 0x00000001U, 0U);
 
     /* one step newton iteration to 53 bits with error less than 0.667 ulps */
     s = t * t;    /* t*t is exact */
