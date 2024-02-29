3678c4336f3fa17c32cb4e1a574aed88328fc1bf
Christoph Weiss <christoph.weiss@gtd-gmbh.de>
2024-02-29 12:39:24 +0100

    Rework integer types in log1p

--- libm/mathf/log1pf.c
+++ libm/mathf/log1pf.c
@@ -19,20 +19,21 @@ float log1pf(float x)
 #endif /* defined(__LIBMCS_FPU_DAZ) */
 
     float hfsq, f, c, R, u;
-    int32_t k, hx, hu, ax;
+    int32_t k;
+    uint32_t hx, hu, ax;
 
     GET_FLOAT_WORD(hx, x);
-    ax = hx & 0x7fffffff;
+    ax = hx & 0x7fffffffU;
 
     k = 1;
 
-    if (!FLT_UWORD_IS_FINITE(hx)) { /* x = NaN/+Inf */
+    if ((int32_t)hx >= 0x7f000000) { /* x = +NaN/+Inf */
         return x + x;
     }
 
-    if (hx < 0x3ed413d7) {          /* x < 0.41422  */
-        if (ax >= 0x3f800000) {     /* x <= -1.0 */
-            if (FLT_UWORD_IS_NAN(ax)) {             /* x = NaN */
+    if ((int32_t)hx < 0x3ed413d7) {          /* x < 0.41422  */
+        if (ax >= 0x3f800000U) {     /* x <= -1.0 */
+            if (FLT_UWORD_IS_NAN(ax)) {             /* x = -NaN */
                 return x + x;
             } else if (x == -1.0f) {
                 return __raise_div_by_zerof(-1.0f); /* log1p(-1)=-inf */
@@ -41,44 +42,44 @@ float log1pf(float x)
             }
         }
 
-        if (ax < 0x31000000) {      /* |x| < 2**-29 */
-            if (ax < 0x24800000) {  /* |x| < 2**-54 */
+        if (ax < 0x31000000U) {      /* |x| < 2**-29 */
+            if (ax < 0x24800000U) {  /* |x| < 2**-54 */
                 return __raise_inexactf(x);
             } else {
                 return __raise_inexactf(x - x * x * 0.5f);
             }
         }
 
-        if (hx > 0 || hx <= ((int32_t)0xbe95f61fU)) {
+        if ((int32_t)hx > 0 || (int32_t)hx <= ((int32_t)0xbe95f61fU)) {
             k = 0;
             f = x;
-            hu = 1;
+            hu = 1U;
         }    /* -0.2929<x<0.41422 */
     }
 
     if (k != 0) {
-        if (hx < 0x5a000000) {
+        if ((int32_t)hx < 0x5a000000) {
             u  = 1.0f + x;
             GET_FLOAT_WORD(hu, u);
-            k  = (hu >> 23) - 127;
+            k  = (int32_t)(hu >> 23U) - 127;
             /* correction term */
             c  = (k > 0) ? 1.0f - (u - x) : x - (u - 1.0f);
             c /= u;
         } else {
             u  = x;
             GET_FLOAT_WORD(hu, u);
-            k  = (hu >> 23) - 127;
-            c  = 0;
+            k  = (int32_t)(hu >> 23U) - 127;
+            c  = 0.0f;
         }
 
-        hu &= 0x007fffff;
+        hu &= 0x007fffffU;
 
-        if (hu < 0x3504f7) {
-            SET_FLOAT_WORD(u, hu | 0x3f800000); /* normalize u */
+        if (hu < 0x3504f7U) {
+            SET_FLOAT_WORD(u, hu | 0x3f800000U); /* normalize u */
         } else {
             k += 1;
-            SET_FLOAT_WORD(u, hu | 0x3f000000); /* normalize u/2 */
-            hu = (0x00800000 - hu) >> 2;
+            SET_FLOAT_WORD(u, hu | 0x3f000000U); /* normalize u/2 */
+            hu = (0x00800000U - hu) >> 2U;
         }
 
         f = u - 1.0f;
@@ -86,21 +87,21 @@ float log1pf(float x)
 
     hfsq = 0.5f * f * f;
 
-    if (hu == 0) { /* |f| < 2**-20 */
+    if (hu == 0U) { /* |f| < 2**-20 */
         if (f == zero) {
-            c += k * ln2_lo;
-            return k * ln2_hi + c;
+            c += (float)k * ln2_lo;
+            return (float)k * ln2_hi + c;
         }
 
         R = hfsq * (1.0f - 0.66666666666666666f * f);
 
-        return k * ln2_hi - ((R - (k * ln2_lo + c)) - f);
+        return (float)k * ln2_hi - ((R - ((float)k * ln2_lo + c)) - f);
     }
 
     if (k == 0) {
         return f - (hfsq - __log1pmff(f));
     } else {
-        return k * ln2_hi - ((hfsq - (__log1pmff(f) + (k * ln2_lo + c))) - f);
+        return (float)k * ln2_hi - ((hfsq - (__log1pmff(f) + ((float)k * ln2_lo + c))) - f);
     }
 }
 
