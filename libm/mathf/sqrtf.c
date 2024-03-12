821e0a79ef8fdd21365efca6cf6c2eb361d89c75
Christoph Weiss <christoph.weiss@gtd-gmbh.de>
2024-03-12 19:11:54 +0100

    Rework integer types in sqrt

--- libm/mathf/sqrtf.c
+++ libm/mathf/sqrtf.c
@@ -12,78 +12,77 @@ float sqrtf(float x)
 #endif /* defined(__LIBMCS_FPU_DAZ) */
 
     float z;
-    uint32_t r, hx;
-    int32_t ix, s, q, m, t, i;
+    uint32_t hx, ix, i, m, q, r, s, t;
 
-    GET_FLOAT_WORD(ix, x);
-    hx = ix & 0x7fffffff;
+    GET_FLOAT_WORD(hx, x);
+    ix = hx & 0x7fffffffU;
 
     /* take care of Inf and NaN */
-    if (!FLT_UWORD_IS_FINITE(hx)) {
-        if (FLT_UWORD_IS_NAN(hx)) { /* sqrt(NaN)=NaN */
+    if (!FLT_UWORD_IS_FINITE(ix)) {
+        if (FLT_UWORD_IS_NAN(ix)) {     /* sqrt(NaN)=NaN */
             return x + x;
-        } else if (ix > 0) {        /* sqrt(+inf)=+inf */
+        } else if ((int32_t)hx > 0) {   /* sqrt(+inf)=+inf */
             return x;
-        } else {                    /* sqrt(-inf)=sNaN */
+        } else {                        /* sqrt(-inf)=sNaN */
             return __raise_invalidf();
         }
     }
 
     /* take care of zero and -ves */
-    if (FLT_UWORD_IS_ZERO(hx)) {
+    if (FLT_UWORD_IS_ZERO(ix)) {
         return x;    /* sqrt(+-0) = +-0 */
     }
 
-    if (ix < 0) {
+    if ((int32_t)hx < 0) {
         return __raise_invalidf();    /* sqrt(-ve) = sNaN */
     }
 
     /* normalize x */
-    m = (ix >> 23);
+    m = (hx >> 23U);
 
-    if (FLT_UWORD_IS_SUBNORMAL(hx)) {       /* subnormal x */
-        for (i = 0; (ix & 0x00800000) == 0; i++) {
-            ix <<= 1;
+    if (FLT_UWORD_IS_SUBNORMAL(ix)) {       /* subnormal x */
+        for (i = 0; (hx & 0x00800000U) == 0U; i++) {
+            hx <<= 1U;
         }
 
-        m -= i - 1;
+        m -= i - 1U;
     }
 
-    m -= 127;    /* unbias exponent */
-    ix = (ix & 0x007fffff) | 0x00800000;
+    m -= 127U;    /* unbias exponent */
+    hx = (hx & 0x007fffffU) | 0x00800000U;
 
-    if (0 < (m & 1)) { /* odd m, double x to make it even */
-        ix += ix;
+    if ((m & 1U) > 0U) { /* odd m, double x to make it even */
+        hx += hx;
     }
 
-    m >>= 1;    /* m = [m/2] */
+    m >>= 1U;    /* m = [m/2] */
 
     /* generate sqrt(x) bit by bit */
-    ix += ix;
-    q = s = 0;        /* q = sqrt(x) */
-    r = 0x01000000;        /* r = moving bit from right to left */
+    hx += hx;
+    q = s = 0U;            /* q = sqrt(x) */
+    r = 0x01000000U;       /* r = moving bit from right to left */
 
-    while (r != 0) {
+    while (r != 0U) {
         t = s + r;
 
-        if (t <= ix) {
+        if (t <= hx) {
             s    = t + r;
-            ix  -= t;
+            hx  -= t;
             q   += r;
         }
 
-        ix += ix;
-        r >>= 1;
+        hx += hx;
+        r >>= 1U;
     }
 
-    if (ix != 0) {
+    if (hx != 0U) {
         (void) __raise_inexactf(x);
-        q += (q & 1);
+        q += (q & 1U);
     }
 
-    ix = (q >> 1) + 0x3f000000;
-    ix += (m << 23);
-    SET_FLOAT_WORD(z, ix);
+    hx = (q >> 1U) + 0x3f000000U;
+    hx += (m << 23U);
+    SET_FLOAT_WORD(z, hx);
     return z;
 }
 
