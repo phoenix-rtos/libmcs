80252f79d486efc7f2e61970055ef4d2321f13a5
Christoph Weiss <christoph.weiss@gtd-gmbh.de>
2024-03-12 17:59:59 +0100

    Rework integer types in tgamma

--- libm/mathf/tgammaf.c
+++ libm/mathf/tgammaf.c
@@ -21,11 +21,11 @@ float tgammaf(float x)
     int signgam_local = 0;
     float y = 0.0f;
 
-    if (isnan(x) != 0) {                        /* tgamma(NaN) = NaN */
+    if (isnan(x) != 0) {                             /* tgamma(NaN) = NaN */
         return x + x;
     } else if (x == 0.0f) {                     /* tgamma(+-0) = +-Inf */
         return __raise_div_by_zerof(x);
-    } else if (floorf(x) == x && x < 0.0f) {    /* tgamma(negative integer, -Inf) = NaN */
+    } else if (floorf(x) == x && x < 0.0f) { 
         return __raise_invalidf();
     } else {
         /* No action required */
