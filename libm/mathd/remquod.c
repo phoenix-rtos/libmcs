5814b10da8c646ef169fc169fad6d7a47547474f
Christoph Weiss <christoph.weiss@gtd-gmbh.de>
2024-03-26 17:11:34 +0100

    Fix spelling issues in comments

--- libm/mathd/remquod.c
+++ libm/mathd/remquod.c
@@ -5,7 +5,7 @@
  *
  * This family of functions implements the floating-point remainder :math:`x\
  * REM\ y` and puts the quotient (or rather its sign and 3 least significant
- * bits) into the outpointer.
+ * bits) into the out pointer.
  *
  * Synopsis
  * ========
