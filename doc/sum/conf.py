c730a36541eb15ca284bf193bf9cc6fdd51f1e5b
Thomas Wucher <thomas.wucher@gtd-gmbh.de>
2025-01-28 14:23:49 +0100

    Doc: Remove unsupported option

--- doc/sum/conf.py
+++ doc/sum/conf.py
@@ -59,7 +59,6 @@ templates_path = ["../_templates"]
 html_theme = "sphinx_rtd_theme"
 
 html_theme_options = {
-    "display_version": True,
     "collapse_navigation": False,
     "logo_only": True,
 }
