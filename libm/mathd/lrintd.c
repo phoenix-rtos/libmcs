61c4eb1be4c928c8e23dc8a06789f64e87e811ca
Andoni Arregi <andoni.arregui@gtd-gmbh.de>
2025-11-17 14:29:13 +0100

    Fix MISRA 12.2 big left shift
    Shifting `1U` left by 31 is not a problem on systems where `1U` is 32
    bits but the shift is undefined on systems where `1U` is 16 bits. This
    is fixed by writing the applied mask explicitly.

--- libm/mathd/lrintd.c
+++ libm/mathd/lrintd.c
@@ -97,7 +97,7 @@ long int lrint(double x)
 
             /* Detect the all-zeros representation of plus and
                minus zero, which fails the calculation below. */
-            if ((_i0 & ~(1U << 31)) == 0) {
+            if ((_i0 & 0x7fffffffU) == 0) {
                 return 0;
             }
 
